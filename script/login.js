41f74210e8e2e98b2df331ff16ec1f7b20c502f4
jadriendev <ddarkknife@gmail.com>
2026-06-23 21:23:25 +0800

    hehe

---
+++ script/login.js
@@ -0,0 +1,58 @@
+const authForm = document.getElementById("authForm");
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+
+togglePassword.addEventListener("click",()=>{
+
+    if(passwordInput.type === "password"){
+
+        passwordInput.type = "text";
+        togglePassword.classList.remove("fa-eye-slash");
+        togglePassword.classList.add("fa-eye");
+
+    }else{
+
+        passwordInput.type = "password";
+        togglePassword.classList.remove("fa-eye");
+        togglePassword.classList.add("fa-eye-slash");
+
+    }
+
+});
+
+
+authForm.addEventListener("submit",(e)=>{
+
+    e.preventDefault();
+
+
+    const email = document.getElementById("email").value;
+    const password = passwordInput.value;
+
+
+    const admin = {
+        email:"demo@gmail.com",
+        password:"demo123"
+    };
+
+
+    if(
+        email === admin.email &&
+        password === admin.password
+    ){
+
+        document.cookie = "adminLogin=true; max-age=86400";
+
+
+        window.location.href = "pages/admin.html";
+
+
+    }else{
+
+        alert("Invalid admin account");
+
+    }
+
+
+});
